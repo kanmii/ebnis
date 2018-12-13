51875ac422b1294a88aa8e353519de2538d85a79
Kanmii <maneptha@gmail.com>
2018-12-13 06:19:54 +0100

    Repo clean up

--- apps/eb_data/lib/eb_data/default_impl/repo.ex
+++ apps/eb_data/lib/eb_data/default_impl/repo.ex
@@ -4,6 +4,6 @@ defmodule EbData.DefaultImpl.Repo do
     adapter: Ecto.Adapters.Postgres
 
   def init(_, opts) do
-      {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
   end
 end
