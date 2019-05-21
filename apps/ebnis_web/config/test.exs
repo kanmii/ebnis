a1e1fbc3be1693d6a31cbd7e131da319d234921a
Adekanmi Ademiiju <maneptha@gmail.com>
2019-05-21 04:03:19 +0200

    Removed hound config.

--- apps/ebnis_web/config/test.exs
+++ apps/ebnis_web/config/test.exs
@@ -1,22 +1,13 @@
 use Mix.Config
 
-app_port = System.get_env("EBNIS_PHOENIX_TEST_PORT") || 4023
-
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :ebnis_web, EbnisWeb.Endpoint,
-  http: [port: app_port],
-  server: true
+  http: [port: 4000],
+  server: false
 
 # Print only warnings and errors during test
 config :logger, level: :warn
-
-config :hound,
-  driver: "chrome_driver",
-  app_host: "http://localhost",
-  app_port: app_port,
-  retry_time: 50_000,
-  genserver_timeout: 480_000
 
 config :eb_data, EbData.DefaultImpl.Repo,
   username: "postgres",
@@ -25,6 +16,5 @@ config :eb_data, EbData.DefaultImpl.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox,
   timeout: 60_000
-
 
 config :eb_emails, EbEmails.DefaultImpl.Mailer, adapter: Swoosh.Adapters.Test
