54d52e671079c34f6dee8c5b8e56832310df9d22
Adekanmi Ademiiju <maneptha@gmail.com>
2019-06-23 20:22:38 +0200

    FieldDef schema - select client ID

--- apps/ebnis_web/lib/ebnis_web/schema/field_def.ex
+++ apps/ebnis_web/lib/ebnis_web/schema/field_def.ex
@@ -18,6 +18,14 @@ defmodule EbnisWeb.Schema.FieldDef do
     @desc "Name of field e.g start, end, meal "
     field(:name, non_null(:string))
 
+    @desc ~S"""
+      String that uniquely identifies this field definition has been
+      created offline. If an associated entry is also created
+      offline, then `createField.defId` **MUST BE** the same as this
+      field and will be validated as such.
+    """
+    field(:client_id, :id)
+
     @desc "The data type of the field"
     field(:type, non_null(:field_type))
   end
