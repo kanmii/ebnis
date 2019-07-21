08d8e0546894055a65a83bed4a569d9554f4344c
Adekanmi Ademiiju <maneptha@gmail.com>
2019-07-21 16:07:27 +0200

    start-server.exe - added --dry option

--- start-server.exs
+++ start-server.exs
@@ -2,10 +2,15 @@
 # running migrations etc, before we start web server during development and test
 # use like so from the command line:
 #   elixir start-server.exs --env=staging [...other_options]
-#   elixir start-server.exs --env=dev --terminal=xterm
-# Other options include:
-#   terminal {string} - your terminal emulator. Defaults to gnome-terminal. But
-#                       only relevant if you are not on windows OS
+#   elixir start-server.exs --prefix='gnome-terminal --'
+#   elixir start-server.exs --env=staging --prefix='tmux -c' --dry
+# Options include:
+#   env {string} - the MIX_ENV of our mix application. Defaults to `dev`
+#   dry {string} - just echo the command, but do not run - useful if you would
+#     like to run the command yourself because you have a complicated use case
+#     but you need the environment variables and iex commands
+#   prefix {string} - e.g. your terminal emulator as shown example above. For
+# windows, this will default to `'start cmd /c`
 
 # load {jason} JSON parser
 # this is assuming you have already compiled this project in dev environment
@@ -25,7 +30,8 @@ Path.expand("_build/dev/lib/jason/ebin", ".")
     System.argv(),
     strict: [
       env: :string,
-      terminal: :string
+      prefix: :string,
+      dry: :boolean
     ]
   )
 
@@ -34,7 +40,8 @@ Path.expand("_build/dev/lib/jason/ebin", ".")
 env = Keyword.get(args, :env, "dev")
 
 # load the environments
-System.put_env(Enum.to_list(envs[env] || []))
+all_envs = Enum.to_list(envs[env] || []) ++ Enum.to_list(envs["email"])
+System.put_env(all_envs)
 
 # execute mix tasks for staging environment
 if env == "staging" do
@@ -43,32 +50,57 @@ if env == "staging" do
   IO.puts([">> ", String.trim(response), "\n"])
 end
 
-# load environment for email during development and tests
-System.put_env(Enum.to_list(envs["email"]))
-
-IO.puts([">> starting server\n"])
-
 # start server in shell
-case :os.type() do
-  {:win32, _} ->
-    case env do
-      "dev" ->
-        :os.cmd('start cmd /c "iex --dot-iex .iexa.exs --werl -S mix phx.server"')
+{command, prefix} =
+  case :os.type() do
+    {:win32, _} ->
+      prefix =
+        Keyword.get(args, :prefix, "start cmd /c")
+        |> String.to_charlist()
 
-      _ ->
-        :os.cmd('start cmd /c "iex -S mix phx.server"')
-    end
+      command =
+        case env do
+          "dev" ->
+            'iex --dot-iex .iexa.exs --werl -S mix phx.server'
+
+          _ ->
+            'iex -S mix phx.server'
+        end
+
+      {command, prefix}
+
+    _ ->
+      prefix =
+        Keyword.get(args, :prefix, "")
+        |> String.to_charlist()
+
+      command =
+        case env do
+          "dev" ->
+            'iex --dot-iex .iexa.exs -S mix phx.server'
+
+          _ ->
+            'iex -S mix phx.server'
+        end
+
+      {command, prefix}
+  end
+
+case Keyword.get(args, :dry) do
+  nil ->
+    composite_command = prefix ++ ' "' ++ command ++ '"'
+
+    IO.puts([">> starting server with command:\n\t", composite_command, "\n"])
+    :os.cmd(composite_command)
 
   _ ->
-    terminal =
-      Keyword.get(args, :terminal, "gnome-terminal")
-      |> String.to_charlist()
-
-    case env do
-      "dev" ->
-        :os.cmd(terminal ++ ' -- iex --dot-iex .iexa.exs -S mix phx.server')
-
-      _ ->
-        :os.cmd(terminal ++ ' -- iex -S mix phx.server')
-    end
+    IO.puts([
+      "\n\n\n",
+      prefix,
+      "\n",
+      Enum.map(all_envs, fn {k, v} -> [k, "=", v, " "] end),
+      "  ",
+      command,
+      "\n\n\n\n"
+    ])
 end
