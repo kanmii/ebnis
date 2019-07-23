46893f7a61ef7e1f33d9a7aa07a254638e7fe192
Adekanmi Ademiiju <maneptha@gmail.com>
2019-07-23 16:20:12 +0200

    Fixing prod issue.

--- apps/ebnis_web/config/prod.exs
+++ apps/ebnis_web/config/prod.exs
@@ -2,10 +2,11 @@ use Mix.Config
 
 config :ebnis_web, EbnisWeb.Endpoint,
   load_from_system_env: true,
+  http: [port: {:system, "PORT"}],
   url: [
     scheme: "https",
     host: "ebnis.herokuapp.com",
-    port: 443
+    port: System.get_env("PORT") |> String.to_integer()
   ],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
