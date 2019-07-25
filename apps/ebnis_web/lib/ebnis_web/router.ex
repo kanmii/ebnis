fb85eb27fb4e488d6565bee03943c8adb80d83f8
Adekanmi Ademiiju <maneptha@gmail.com>
2019-07-25 16:50:49 +0200

    Moved E2eController to router.ex and compile it only in e2e env.

--- apps/ebnis_web/lib/ebnis_web/router.ex
+++ apps/ebnis_web/lib/ebnis_web/router.ex
@@ -36,3 +36,36 @@ defmodule EbnisWeb.Router do
     )
   end
 end
+
+if Mix.env() == :e2e do
+  defmodule EbnisWeb.E2eController do
+    use EbnisWeb, :controller
+
+    def reset_db(conn, _) do
+      case EbData.DefaultImpl.Repo.reset_db() do
+        {:ok, _} ->
+          resp(conn, 200, "ok")
+
+        _ ->
+          resp(conn, 400, "error")
+      end
+    end
+
+    def create_user(conn, %{"user" => user_creation_params}) do
+      alias EbData.Guardian, as: AppGuardian
+
+      with {:ok, user} <- EbData.register(user_creation_params),
+           {:ok, jwt, _claim} <- AppGuardian.encode_and_sign(user) do
+        json(
+          conn,
+          user
+          |> Map.take([:id, :email, :name])
+          |> Map.put(:jwt, jwt)
+        )
+      else
+        _ ->
+          json(conn, %{error: true})
+      end
+    end
+  end
+end
