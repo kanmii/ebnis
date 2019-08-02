4125ebd645e487687d96c4167cc6249fc5def17a
Adekanmi Ademiiju <maneptha@gmail.com>
2019-08-02 19:49:01 +0200

    Entry.field updated - removed utility function

--- apps/ebnis/lib/ebnis/release.ex
+++ apps/ebnis/lib/ebnis/release.ex
@@ -13,32 +13,6 @@ defmodule Ebnis.Release do
     end
   end
 
-  def assign_ids_to_entries_fields do
-    alias EbnisData.Entry
-    alias EbnisData.Field
-    alias EbnisData.Repo
-
-    import Ecto.Query, warn: false
-
-    stream =
-      Repo.stream(Entry)
-      |> Stream.map(fn entry ->
-        fields =
-          entry.fields
-          |> Enum.map(&%Field{&1 | id: Ecto.UUID.generate()})
-
-        Entry
-        |> where([e], e.id == ^entry.id)
-        |> select([e], e.fields)
-        |> update([], set: [fields: ^fields])
-        |> Repo.update_all([])
-      end)
-
-    Repo.transaction(fn ->
-      Enum.to_list(stream)
-    end)
-  end
-
   def rollback(repo, version) do
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
