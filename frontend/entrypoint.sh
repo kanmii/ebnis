ba32bd30a900a63c694097629db3a77f51604dbe
Kanmii <maneptha@gmail.com>
2020-08-14 15:43:14 +0200

    docker web should not depend on backend.

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-wait-until "curl --output /dev/null --silent --head --fail http://api:${PORT}${BACKEND_HEALTH_CHECK_URL}"
+# wait-until "curl --output /dev/null --silent --head --fail http://api:${PORT}${BACKEND_HEALTH_CHECK_URL}"
 
 yarn start dev
