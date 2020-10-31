8612c777f33b98692b7023ed9875c4200c9017f9
Kanmii <maneptha@gmail.com>
2020-10-31 20:43:00 +0100

    chore: add documentation to end to end test - My.spec

--- frontend/cypress/integration/My.spec.ts
+++ frontend/cypress/integration/My.spec.ts
@@ -29,7 +29,8 @@ context("My page", () => {
   const newExperienceTitle = existingExperienceTitle + newExperienceTitleAppend;
 
   describe("online experience", () => {
-    it("create fails/succeeds", () => {
+    it("create fails/succeeds/update", () => {
+      // Given an online experience exists in the system
       const p = createOnlineExperience({
         title: existingExperienceTitle,
         description: "dd",
@@ -42,44 +43,69 @@ context("My page", () => {
       });
 
       cy.wrap(p).then(() => {
+        // When we visit experiences list page
         cy.visit(MY_URL);
-        cy.title().should("contain", MY_TITLE);
-        cy.get("#" + activateInsertExperienceDomId).click();
-        cy.get("#" + newExperienceDomId).then(() => {
-          cy.get("#" + titleInputDomId).type(existingExperienceTitle);
-          cy.get("#" + descriptionInputDomId).type("dd"); // existing title
 
+        // We should see page title
+        cy.title().should("contain", MY_TITLE);
+
+        // When link to create new experience is clicked
+        cy.get("#" + activateInsertExperienceDomId).click();
+
+        // We should see UI to create new experience
+        cy.get("#" + newExperienceDomId).then(() => {
+          // When we fill title field with existing experience title
+          cy.get("#" + titleInputDomId).type(existingExperienceTitle);
+
+          // And we fill description field
+          cy.get("#" + descriptionInputDomId).type("dd");
+
+          // And we fill data definition name
           cy.get("." + definitionNameFormControlSelector).type("n0");
 
+          // And we complete data type
           cy.get("." + definitionTypeFormControlSelector).select(
             DataTypes.SINGLE_LINE_TEXT,
           );
 
+          // Notification that experience created successfully/failed should not be visible
           cy.get("#" + notificationCloseId).should("not.exist");
+
+          // When form is submitted
           cy.get("#" + submitDomId).click();
+
+          // Notification that experience creation failed should be visible
           cy.get("#" + notificationCloseId)
             .should("exist")
+            // When the notification is closed
             .click();
+
+          // Notification should not be visible
           cy.get("#" + notificationCloseId).should("not.exist");
 
           ////////////////////////// new experience ///////////////////////
 
+          // When form is filled with a title that did not exist in the system
           cy.get("#" + titleInputDomId).type(newExperienceTitleAppend);
 
-          // add dataDefinition field
+          // And additional data definition field is added
           cy.get("." + addDefinitionSelector).click();
 
+          // And definition name field is completed
           cy.get("." + definitionNameFormControlSelector)
             .eq(1)
             .type("n1");
 
+          // And definition type is completed
           cy.get("." + definitionTypeFormControlSelector)
             .eq(1)
             .select(DataTypes.SINGLE_LINE_TEXT);
 
+          // And form is submitted
           cy.get("#" + submitDomId).click();
         });
 
+        // then we should be redirected to newly created experience page
         cy.title().should("contain", newExperienceTitle);
       });
     });
@@ -87,6 +113,7 @@ context("My page", () => {
 
   describe("offline experience", () => {
     it("create fails/succeeds", () => {
+      // Given there is an offline experience in the system
       const p = createOfflineExperience(
         {
           input: [
@@ -106,31 +133,54 @@ context("My page", () => {
       );
 
       cy.wrap(p).then(() => {
+        // When we visit experiences list page
         cy.visit(MY_URL);
+
+        // We should see the page title
         cy.title().should("contain", MY_TITLE);
+
+        // When we click link to create new experience
         cy.get("#" + activateInsertExperienceDomId).click();
+
+        // And connection to our backend is severed
         cy.setConnectionStatus(false);
 
+        // We should see UI to create new experience
         cy.get("#" + newExperienceDomId).then(() => {
+          // When we fill title field with existing experience title
           cy.get("#" + titleInputDomId).type(existingExperienceTitle);
+
+          // And we fill description field
           cy.get("#" + descriptionInputDomId).type("dd");
 
+          // And we fill data definition name
           cy.get("." + definitionNameFormControlSelector).type("n0");
 
+          // And we complete data type
           cy.get("." + definitionTypeFormControlSelector).select(
             DataTypes.SINGLE_LINE_TEXT,
           );
 
+          // Notification that experience created successfully/failed should not be visible
           cy.get("#" + notificationCloseId).should("not.exist");
+
+          // When form is submitted
           cy.get("#" + submitDomId).click();
+
+          // Notification that experience creation failed should be visible
           cy.get("#" + notificationCloseId).should("exist");
 
           ////////////////////////// new experience ///////////////////////
 
+          // When form is filled with a title that did not exist in the system
           cy.get("#" + titleInputDomId).type(newExperienceTitleAppend);
+
+          // And form is submitted
           cy.get("#" + submitDomId).click();
         });
-        // cy.title().should("contain", newExperienceTitle);
+
+        // then we should be redirected to newly created experience page
+        cy.title().should("contain", newExperienceTitle);
       });
     });
   });
