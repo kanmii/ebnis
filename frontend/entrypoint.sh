15801a8a38b43bb401f4717707fe4ffdd02196c6
Kanmii <maneptha@gmail.com>
2021-01-28 17:31:19 +0100

    js: entrypoint: extract install yarn to function

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -2,13 +2,22 @@
 
 set -e
 
-# wait-until "curl --output /dev/null --silent --head --fail http://api:${PORT}${BACKEND_HEALTH_CHECK_URL}"
+app="cra"
 
-if ping -q -c 1 -W 1 google.com >/dev/null; then
-  yarn install
-fi
+function yarnInstall {
+  if ping -q -c 1 -W 1 google.com >/dev/null; then
+    echo -e "\nFetching and building node packages for app: $app."
+    echo -e "Running: 'yarn install --frozen-lockfile'\n"
+
+    yarn install --frozen-lockfile
+
+    echo -e "\nDone running: 'yarn install --frozen-lockfile' for app: $app\n"
+  fi
+}
 
 # Create react app inotify issue
 echo fs.inotify.max_user_watches=524288 | tee -a /etc/sysctl.conf && sysctl -p
+
+yarnInstall;
 
 yarn start cra
